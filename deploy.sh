e8779719dc75e1702f07c0a7ffbd2fc7ab0b6720
Redips <joker528@icloud.com>
2017-06-29 10:15:05 +0800

    v2.1.1 add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,30 @@
+cp -r projects ./projects
+cd ./projects/bodyExtract/
+scrapyd-deploy default -p bodyExtract
+
+cd ../DoubanBookSpider/
+scrapyd-deploy default -p DoubanBookSpider
+
+cd ../JDSearch/
+scrapyd-deploy default -p JDSearch
+
+cd ../jingdongModel/
+scrapyd-deploy default -p jingdongModel
+
+cd ../ProxyCrawler/
+scrapyd-deploy default -p ProxyCrawler
+
+cd ../ProxyTester/
+scrapyd-deploy default -p ProxyTester
+
+cd ../taobaoCrawler/
+scrapyd-deploy default -p taobaoCrawler
+
+cd ../taobaoSearch/
+scrapyd-deploy default -p taobaoSearch
+
+cd ../yhdCrawler/
+scrapyd-deploy default -p yhdCrawler
+
+cd ../yhdSearch
+scrapyd-deploy default -p yhdSearch
